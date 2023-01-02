106840afc5f7b3de4085ff87c0ca412e96273742
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-01-02 13:36:21 +0700

    add util functions for jwt and some demo

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,5 +1,5 @@
 from rest_framework_jwt.settings import api_settings
-
+from rest_framework_jwt.utils import jwt_decode_handler
 def generate_jwt_token(user, data):
     jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
@@ -7,3 +7,15 @@ def generate_jwt_token(user, data):
     token = jwt_encode_handler(payload)
     data['token'] = token
     return data
+
+def validate_access_token(access_token):
+    try:
+        token = jwt_decode_handler(access_token)
+        return True
+    except Exception:
+        return False
+    
+def get_user_id_from_token(access_token):
+    token = jwt_decode_handler(access_token)
+    user_id = token['user_id']
+    return user_id
