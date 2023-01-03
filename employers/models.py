9ac410d4a7c5e18bde92897d43666e407d58205e
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-01-03 11:59:41 +0700

    add name field and some shit idk

--- employers/models.py
+++ employers/models.py
@@ -4,6 +4,7 @@ from companies.models import Company
 # Create your models here.
 
 class Employer(models.Model):
+    name = models.CharField(max_length=100)
     email = models.EmailField(_('email address'), unique=True)
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
     
