d1fe0bd9df466a005879772734a0eda6ca8bff76
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-01-11 14:02:06 +0700

    add relationships

--- observers/models.py
+++ observers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from students.models import Student
+from companies.models import Company
 # Create your models here.
+class Follow(models.Model):
+    date_followed = models.DateTimeField(auto_now_add=True)
+    # delete follow if one of the student or company is deleted.
+    student = models.ForeignObject(Student, on_delete=models.CASCADE)
+    company = models.ForeignObject(Company, on_delete=models.CASCADE)
